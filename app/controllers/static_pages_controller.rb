c51eb67e20c64239659e42ce335c8b158c6a2c9f
Rafał Kęsik <kesik.rafal@yahoo.com>
2024-11-15 14:49:01 +0100

    Add layout and sample static pages.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,18 @@ class StaticPagesController < ApplicationController
   
   def home
   end
+
+  def guidelines
+  end
+
+  def faq
+  end
+
+  def contact
+  end
+
+  def empty_site
+    flash[:warning] = "The requested page is still in progress."
+    redirect_to root_url
+  end
 end
