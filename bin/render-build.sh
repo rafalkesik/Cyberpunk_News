aa754964e0b03d99e4e907863eab48c851bae24a
Rafał Kęsik <kesik.rafal@yahoo.com>
2024-12-02 12:14:58 +0100

    Refactor: 1. Prevent a bug in case a logged-in user is deleted. 2. Turn off database reset in production

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,9 +6,10 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
+# In case we want to reset the database, uncomment the following line:
+# bundle exec rails db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
-
-bundle exec rails db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rails db:migrate
