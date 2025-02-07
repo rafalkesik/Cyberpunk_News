cc4c4332531645538becde45efa5f78d1478d8e5
Rafal Kesik <rafal.kesik.g@gmail.com>
2025-02-07 14:42:18 +0100

    fix mailer authentication v2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: 587,
-    domain: 'onrender.com',
+    domain: 'sendgrid.net',
     authentication: 'plain',
     enable_starttls_auto: true,
     user_name: ENV['SENDGRID_USERNAME'],
