13349c4ca4e1730bbf7e51bc21261a86784b7d0b
Rafal Kesik <rafal.kesik.g@gmail.com>
2025-02-07 21:37:28 +0100

    fix mailer authentication v4

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -6,21 +6,21 @@ class ApplicationMailer < ActionMailer::Base
   layout 'mailer'
 
   def send_email(to, subject, content)
-    from = Email.new(email: 'no-reply@yourdomain.com') # Verified "From" email
+    from = Email.new(email: 'rafal.kesik.g@gmail.com') # Verified "From" email
     to = Email.new(email: to)
     content = Content.new(type: 'text/html', value: content) # Or 'text/plain' for plain text
     mail = Mail.new(from, subject, to, content)
 
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    Rails.logger.info "Sending email via SendGrid..."
     response = sg.client.mail._('send').post(request_body: mail.to_json)
+    Rails.logger.info "SendGrid Response Status: #{response.status_code}"
 
     # You can handle the response if needed
-    puts response.status_code
-    puts response.body
-    puts response.headers
-    Rails.logger.info "SendGrid Response: #{response.status_code}"
     return if response.status_code == '202'
 
-    Rails.logger.error "Error sending email: #{response.body}"
+    Rails.logger.error "Error sending email via SendGrid: Status #{response.status_code}"
+    Rails.logger.error "Response Body: #{response.body}"
+    Rails.logger.error "Response Headers: #{response.headers}"
   end
 end
